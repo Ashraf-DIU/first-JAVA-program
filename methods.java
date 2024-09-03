c75d4ccd511bd1f2f8839deed648a1f90e2a6dff
Ashraf-DIU <ashrafbinalam17@gmail.com>
2024-09-03 11:31:14 +0600

    adding methods

---
+++ methods.java
@@ -0,0 +1,30 @@
+public class methods {
+    public static void printJ(){
+        System.out.println("Hello Java");
+    }
+    public static void printName (String name){
+        System.out.println(name);
+    }
+    public static void printSum(int a, int b){
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        //Methods
+        printJ();
+        printName("Aman");
+        printName("Akku");
+        printSum(6, 7);
+        
+        //Try-Match in expectations
+        // int [] marks = {98, 97, 78};
+        // try {
+        //     System.out.println(marks[5]);
+        // } catch (Exception e) {
+        //     //Do something after catching
+        // }
+        // System.out.println(marks[1]);
+        // System.out.println("My name is Aman");
+    }
+}
